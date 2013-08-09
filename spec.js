8033e3d19df1138172decd00b44b4ea842d1615f
Kory Nunn <knunn187@gmail.com>
2013-08-09 10:18:52 +1000

    initial commit

---
+++ spec.js
@@ -0,0 +1,35 @@
+Object.create = Object.create || function (o) {
+    if (arguments.length > 1) {
+        throw new Error('Object.create implementation only accepts the first parameter.');
+    }
+    function F() {}
+    F.prototype = o;
+    return new F();
+};
+
+function createSpec(child, parent){
+    var parentPrototype;
+    
+    if(!parent) {
+        parent = Object;
+    }
+    
+    if(!parent.prototype) {
+        parent.prototype = {};
+    }
+    
+    parentPrototype = parent.prototype;
+    
+    child.prototype = Object.create(parent.prototype);
+    child.prototype.__super__ = parentPrototype;
+    
+    // Yes, This is 'bad'. However, it runs once per Spec creation.
+    var spec = new Function("child", "return function " + child.name + "(){child.prototype.__super__.constructor.apply(this, arguments);return child.apply(this, arguments);}")(child);
+    
+    spec.prototype = child.prototype;
+    spec.prototype.constructor = child.prototype.constructor = spec;
+    
+    return spec;
+}
+
+module.exports = createSpec;
---
+++ test/test.js
@@ -0,0 +1,63 @@
+var test = require('tape'),
+    createSpec = require('../');
+
+
+//Base
+function Shape(){
+    this.sides = null;
+}
+Shape = createSpec(Shape);
+
+
+//Inherits from Shape
+function Square(){
+    this.sides = 4;
+}
+Square = createSpec(Square, Shape);
+
+
+//Inherits from Square
+function RoundedSquare(){
+    this.cornerRadius = 10;
+}
+RoundedSquare = createSpec(RoundedSquare, Square);
+
+
+
+test('inherit from parent', function (t) {
+    t.plan(4);
+
+    var shape = new Shape();
+    
+    t.ok(shape instanceof Object);
+    t.ok(shape instanceof Shape);
+    t.ok('sides' in shape);
+    t.ok(shape.sides === null);
+    t.end();
+});
+
+test('inherit from inherited parent', function (t) {
+    t.plan(4);
+
+    var square = new Square();
+    
+    t.ok(square instanceof Object);
+    t.ok(square instanceof Shape);
+    t.ok(square instanceof Square);
+    t.ok(square.sides === 4);
+    t.end();
+});
+
+test('inherit from object', function (t) {
+    t.plan(6);
+
+    var roundedSquare = new RoundedSquare();
+    
+    t.ok(roundedSquare instanceof Object);
+    t.ok(roundedSquare instanceof Shape);
+    t.ok(roundedSquare instanceof Square);
+    t.ok(roundedSquare instanceof RoundedSquare);
+    t.ok(roundedSquare.sides === 4);    
+    t.ok(roundedSquare.cornerRadius === 10);
+    t.end();
+});
